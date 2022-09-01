bb391e221bbdf1d450f0e84c25c356fae0f97ea9
Elijah Hendrickson <e.hendy21@gmail.com>
2022-09-01 18:27:12 -0500

    Big Inversion

--- put-together/src/components/PersonCard.jsx
+++ put-together/src/components/PersonCard.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 
-export default class PersonCard extends Component {
-    constructor(props){
-        super(props);
-            this.state = {
-                age : this.props.age
-            }
-    }
+// export default class PersonCard extends Component {
+//     constructor(props){
+//         super(props);
+//             this.state = {
+//                 age : this.props.age
+//             }
+//     }
 
-    render() {
-        return (
-            <div>
-                <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.state.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-                <button onClick={() => this.setState({age : this.state.age + 1})}>Bithday Button for {this.props.firstName} {this.props.firstName}</button>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.firstName}, {this.props.lastName}</h1>
+//                 <p>Age: {this.state.age}</p>
+//                 <p>Hair Color: {this.props.hairColor}</p>
+//                 <button onClick={() => this.setState({age : this.state.age + 1})}>Bithday Button for {this.props.firstName} {this.props.firstName}</button>
+//             </div>
+//         )
+//     }
+// }
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </div>
+    )
 }
+export default PersonCard;
